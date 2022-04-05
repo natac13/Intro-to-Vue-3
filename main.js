5052f43c70aeafda778c71b6baab41ca3d9ba1da
Sean Campbell <sean.campbell13@gmail.com>
2022-04-05 11:00:07 -0400

    complete L2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description:
+        "Dolore voluptate id minim eu in nulla culpa excepteur incididunt in nostrud duis.",
+    };
+  },
+});
